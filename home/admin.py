c8e9169ea3d76817d435dc9f07ab4740a3093174
Shivam Patel <shivam@tenancy360.co.uk>
2023-02-19 14:17:07 +0000

    Added knockout table and points per game to models

--- home/admin.py
+++ home/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Sport, Chapter, FootballTeam, FootballPitch, \
-    FootballSchedule, \
-    FootballTable
+from .models import Sport, Chapter, KnockoutStep, FootballTeam, FootballPitch, \
+    FootballSchedule, FootballTable, FootballKnockout
 
 admin.site.register(Sport)
 admin.site.register(Chapter)
+admin.site.register(KnockoutStep)
 admin.site.register(FootballTeam)
 admin.site.register(FootballPitch)
 admin.site.register(FootballSchedule)
 admin.site.register(FootballTable)
+admin.site.register(FootballKnockout)
--- home/models.py
+++ home/models.py
@@ -22,6 +22,14 @@ class Chapter(models.Model):
         return self.name
 
 
+class KnockoutStep(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class FootballTeam(models.Model):
     team_id = models.AutoField(primary_key=True, db_index=True)
     name = models.CharField(max_length=200)
@@ -52,7 +60,67 @@ class FootballSchedule(models.Model):
     opponent_score = models.IntegerField(null=True)
     played = models.BooleanField(default=False)
     time = models.TimeField(null=True)
-    pitch_id = models.ForeignKey(FootballPitch, on_delete=models.CASCADE, null=True)
+    pitch = models.ForeignKey(FootballPitch, on_delete=models.CASCADE, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.team.name} vs {self.opponent.name}"
+
+    @property
+    def get_pitch(self):
+        return self.pitch.name
+
+    @property
+    def get_time(self):
+        return self.time.strftime("%H:%M")
+
+    @property
+    def get_team(self):
+        return self.team.name
+
+    @property
+    def get_opponent(self):
+        return self.opponent.name
+
+    @property
+    def get_result(self):
+        if self.team_score is None or self.opponent_score is None:
+            return False
+        else:
+            return f"{self.team_score} - {self.opponent_score}"
+
+
+class FootballTable(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True)
+    team_id = models.ForeignKey(FootballTeam, on_delete=models.CASCADE)
+    played = models.IntegerField()
+    won = models.IntegerField()
+    drawn = models.IntegerField()
+    lost = models.IntegerField()
+    goals_for = models.IntegerField()
+    goals_against = models.IntegerField()
+    goal_difference = models.IntegerField()
+    points = models.IntegerField()
+    points_per_game = models.DecimalField()
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.team_id.name
+
+
+class FootballKnockout(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True)
+    team = models.ForeignKey(FootballTeam, on_delete=models.CASCADE)
+    opponent = models.ForeignKey(FootballTeam, on_delete=models.CASCADE,
+                                    related_name="opponent_id")
+    team_score = models.IntegerField(null=True)
+    opponent_score = models.IntegerField(null=True)
+    played = models.BooleanField(default=False)
+    team_penalty = models.IntegerField(null=True)
+    opponent_penalty = models.IntegerField(null=True)
+    time = models.TimeField()
+    pitch_id = models.ForeignKey(FootballPitch, on_delete=models.CASCADE)
+    step = models.ForeignKey(KnockoutStep, on_delete=models.CASCADE)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
@@ -82,35 +150,3 @@ class FootballSchedule(models.Model):
             return f"{self.team_score} - {self.opponent_score} \n (Penalties: {self.team_penalty} - {self.opponent_penalty}) "
         else:
             return f"{self.team_score} - {self.opponent_score}"
-
-
-class FootballTable(models.Model):
-    id = models.AutoField(primary_key=True, db_index=True)
-    team_id = models.ForeignKey(FootballTeam, on_delete=models.CASCADE)
-    played = models.IntegerField()
-    won = models.IntegerField()
-    drawn = models.IntegerField()
-    lost = models.IntegerField()
-    goals_for = models.IntegerField()
-    goals_against = models.IntegerField()
-    goal_difference = models.IntegerField()
-    points = models.IntegerField()
-    is_deleted = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.team_id.name
-
-
-class FootballKnockout(models.Model):
-    id = models.AutoField(primary_key=True, db_index=True)
-    team_id = models.ForeignKey(FootballTeam, on_delete=models.CASCADE)
-    opponent_id = models.ForeignKey(FootballTeam, on_delete=models.CASCADE,
-                                    related_name="opponent_id")
-    team_score = models.IntegerField(null=True)
-    opponent_score = models.IntegerField(null=True)
-    played = models.BooleanField(default=False)
-    team_penalty = models.IntegerField(null=True)
-    opponent_penalty = models.IntegerField(null=True)
-    time = models.TimeField()
-    pitch_id = models.ForeignKey(FootballPitch, on_delete=models.CASCADE)
-    is_deleted = models.BooleanField(default=False)
